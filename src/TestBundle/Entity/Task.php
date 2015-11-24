562679d9590b2696e8fd5528d8c1a47f8a84cfe6
shadows05 <gaoduntao@gmail.com>
2015-11-24 22:02:36 +0800

    form

--- src/TestBundle/Entity/Task.php
+++ src/TestBundle/Entity/Task.php
@@ -8,8 +8,37 @@
 
 namespace TestBundle\Entity;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 class Task
 {
+    /**
+     * @Assert\NotBlank()
+     */
+    protected $task;
 
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Type("\DateTime")
+     */
+    protected $dueDate;
+
+    public function getTask()
+    {
+        return $this->task;
+    }
+    public function setTask($task)
+    {
+        $this->task = $task;
+    }
+
+    public function getDueDate()
+    {
+        return $this->dueDate;
+    }
+    public function setDueDate(\DateTime $dueDate = null)
+    {
+        $this->dueDate = $dueDate;
+    }
 }
