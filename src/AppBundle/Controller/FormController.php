36cb498eac759d04fab61019ef6eea34fc0c2699
unknown <gaoduntao@1a6p-D9994050.lefeng.max>
2015-11-25 09:43:10 +0800

    change ignore file

---
+++ src/AppBundle/Controller/FormController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gaoduntao
+ * Date: 2015/11/23
+ * Time: 11:10
+ */
+
+namespace AppBundle\Controller;
+
+
+class FormController
+{
+
+}
---
+++ src/AppBundle/Entity/Task.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gaoduntao
+ * Date: 2015/11/23
+ * Time: 11:11
+ */
+
+namespace AppBundle\Entity;
+
+
+class Task
+{
+
+}
